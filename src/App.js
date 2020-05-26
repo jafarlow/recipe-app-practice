6882a1c0a69080f941c9df50c0a332d3f1f9f80e
jafarlow <jacobafarlow@gmail.com>
2020-05-26 17:58:07 -0400

    Started basic structure for app
    App.js
      - removed logo import
      - wrote calls to .env for keys
      - wrote sample GET request using API docs
      - replaced placeholder `<h1>` with simple form structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const apiKey = process.env.REACT_APP_API_KEY
+  const appID = process.env.REACT_APP_RECIPE_APP_ID
+
+  // sample GET request:
+  //"https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
+  const request = `https://api.edamam.com/search?q=chicken&app_id=${appID}&app_key=${apiKey}`
+
   return (
     <div className="App">
-      <h1>HUMANS!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
