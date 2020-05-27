a7fd18b5e5122b030d30a030ea66456cd0f749b2
jafarlow <jacobafarlow@gmail.com>
2020-05-26 21:04:13 -0400

    Begin building search function
    App.js
      - wrote search/setSearch & query/setQuery with useState
        - moved this grouping above the `request` since my fetch URL is
          currently outside my `getRecipes()`
      - started an `updateSearch()` which uses `setSearch`
      - added "value" & "onChange" to `<input>`

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ function App() {
   const apiKey = process.env.REACT_APP_API_KEY
   const appID = process.env.REACT_APP_RECIPE_APP_ID
 
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
+
   // sample GET request:
   //"https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
-  const request = `https://api.edamam.com/search?q=chicken&app_id=${appID}&app_key=${apiKey}`
-
-  const [recipes, setRecipes] = useState([])
+  const request = `https://api.edamam.com/search?q=${query}&app_id=${appID}&app_key=${apiKey}`
 
   useEffect(() => {
     getRecipes()
@@ -24,10 +26,14 @@ function App() {
     console.log(data.hits);
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
   return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text" />
+        <input className="search-bar" type="text" value={saerch} onChange={updateSearch}/>
         <button className="search-button" type="submit">Search</button>
       </form>
       {recipes.map(dish => (
