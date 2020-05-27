87cbc1df82809ada8126860de864e77ac76cb2cd
jafarlow <jacobafarlow@gmail.com>
2020-05-26 20:37:30 -0400

    Started backbone of Recipe component
    Recipe.js
      - placeholder elements for title, calories, and recipe image all
        wrapped inside a single `<div>`

---
+++ src/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Recipe = () => {
+  return (
+    <div>
+      <h2>Title</h2>
+      <p>Calories</p>
+      <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Recipe
