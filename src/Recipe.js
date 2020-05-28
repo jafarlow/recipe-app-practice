4807193034152fae48b65b88709d7208158b9252
jafarlow <jacobafarlow@gmail.com>
2020-05-28 04:15:07 -0400

    Reordered dish layout
    Recipe.js
      - moved the image to be immediately beneath the recipe title

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,13 +4,13 @@ const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div className="dish">
       <h2>{title}</h2>
+      <img src={image} alt="" />
       <ul>
         {ingredients.map(ingredient => (
           <li>{ingredient.text}</li>
         ))}
       </ul>
       <p>{calories}</p>
-      <img src={image} alt="" />
     </div>
   )
 }
