4180ae022fc716c8c6cbc77455b90b04757d4cb4
jafarlow <jacobafarlow@gmail.com>
2020-05-28 15:54:08 -0400

    Added key to ingredient
    Recipe.js
      - when mapping through the ingredients list, React flags a console
        warning that they do not have unique keys, and that I should
        resolve that
        - after much trial and error, and digging to no avail, I settled
          on generating the key using `Math.random()`
        - wrote a note explaining why in case I forget when I review this
          code later

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+// NOTE: the API doesn't supply an ID for ingredients, so we're using
+// Math.random() to generate a unique ID not tied to index. This
+// generates a number with 16 digits, which means the likelihood of
+// generating a duplicate value in the list is small enough to
+// consider it 0%
+
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div className="dish">
@@ -7,7 +13,7 @@ const Recipe = ({ title, calories, image, ingredients }) => {
       <img src={image} alt="" />
       <ul>
         {ingredients.map(ingredient => (
-          <li>{ingredient.text}</li>
+          <li key={Math.random()}>{ingredient}</li>
         ))}
       </ul>
       <p>{calories}</p>
