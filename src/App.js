7830ac4df38e456e7306163de002fe81ff4f5420
jafarlow <jacobafarlow@gmail.com>
2020-06-03 05:19:50 -0400

    Incorporating react router
    App.js
      - imported all the generic defaults for react router
      - imported the two new components
      - replaced all info in the `return` with the Router syntax
        - link to home & search
        - routes to match
      - commented out all the earlier function code as it was all related
        to searching
        - once everything is confirmed to be operating as expected, this
          code will be fully removed

--- src/App.js
+++ src/App.js
@@ -1,63 +1,70 @@
 import React, { useEffect, useState } from 'react';
-import Recipe from './Recipe.js'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home.js'
+import Search from './Search.js'
+
+// import Recipe from './Recipe.js'
 
 function App() {
-
-  const apiKey = process.env.REACT_APP_API_KEY
-  const appID = process.env.REACT_APP_RECIPE_APP_ID
-
-  const [recipes, setRecipes] = useState([])
-  const [search, setSearch] = useState('')
-  const [query, setQuery] = useState('')
-
-  // sample GET request:
-  //"https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
-  const request = `https://api.edamam.com/search?q=${query}&app_id=${appID}&app_key=${apiKey}`
-
-  useEffect(() => {
-    getRecipes()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query])
-
-  const getRecipes = async () => {
-    const response = await fetch(request)
-    const data = await response.json()
-    // hits are the individual recipes returned from a query
-    setRecipes(data.hits)
-    console.log(data.hits);
-  }
-
-  const updateSearch = e => {
-    setSearch(e.target.value)
-  }
-
-  const getSearch = e => {
-    e.preventDefault()
-    setQuery(search)
-    setSearch('')
-  }
+  //
+  // const apiKey = process.env.REACT_APP_API_KEY
+  // const appID = process.env.REACT_APP_RECIPE_APP_ID
+  //
+  // const [recipes, setRecipes] = useState([])
+  // const [search, setSearch] = useState('')
+  // const [query, setQuery] = useState('')
+  //
+  // // sample GET request:
+  // //"https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
+  // const request = `https://api.edamam.com/search?q=${query}&app_id=${appID}&app_key=${apiKey}`
+  //
+  // useEffect(() => {
+  //   getRecipes()
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [query])
+  //
+  // const getRecipes = async () => {
+  //   const response = await fetch(request)
+  //   const data = await response.json()
+  //   // hits are the individual recipes returned from a query
+  //   setRecipes(data.hits)
+  //   console.log(data.hits);
+  // }
+  //
+  // const updateSearch = e => {
+  //   setSearch(e.target.value)
+  // }
+  //
+  // const getSearch = e => {
+  //   e.preventDefault()
+  //   setQuery(search)
+  //   setSearch('')
+  // }
 
   return (
-    <div className="App">
-      <h1>Recipe Search</h1>
-      <form className="search-form" onSubmit={getSearch}>
-        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
-        <button className="search-button" type="submit">Search</button>
-      </form>
-      <article className="recipes">
-        {recipes.map(dish => (
-          <Recipe
-            key={dish.recipe.calories}
-            title={dish.recipe.label}
-            calories={dish.recipe.calories}
-            image={dish.recipe.image}
-            ingredients={dish.recipe.ingredientLines}
-            servings={dish.recipe.yield}
-            url={dish.recipe.url}
-          />
-        ))}
-      </article>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Recipe Search</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
