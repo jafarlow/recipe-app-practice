9249bec491aaec4d4143eca82de643838907e958
jafarlow <jacobafarlow@gmail.com>
2020-06-04 02:34:19 -0400

    Added copy to `Home` component
    Home.js
      - Wrote two paragraphs describing the app, as well as its 5 searches
        per minute limitation
      - this copy will likely need to be refined

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <div className="Home">
       <h1>Home</h1>
+      <h2>About the app:</h2>
+      <p>This is a recipe search app which uses the Edamam API to help you find new dishes to cook. This is a great way to quickly scan a recipe's ingredients to help you determine if you've found your next favorite dish! Each recipe includes a link to its source page where you can find the cooking instructions. Just imagine, finding the ingredients for a recipe <em><strong>before</strong></em> having to scroll past those stories!</p>
+      <p>This app uses the free tier of the Edamam API, which limits searches per minute to 5. If you encounter any difficulty returning recipes from your search, hang tight for a second before initiating another search.</p>
     </div>
   )
 }
