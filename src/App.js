83d83cc8156c5f5101ddae68dbfe64d53984a658
jafarlow <jacobafarlow@gmail.com>
2020-06-04 17:06:36 -0400

    Removed commented code from App
    App.js
      - code which had been shifted over to Search, but remained commented
        out in App has been removed
      - removed Recipe component import since that has also been moved to
        the Search component

--- src/App.js
+++ src/App.js
@@ -3,43 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './Home.js'
 import Search from './Search.js'
 
-// import Recipe from './Recipe.js'
-
 function App() {
-  //
-  // const apiKey = process.env.REACT_APP_API_KEY
-  // const appID = process.env.REACT_APP_RECIPE_APP_ID
-  //
-  // const [recipes, setRecipes] = useState([])
-  // const [search, setSearch] = useState('')
-  // const [query, setQuery] = useState('')
-  //
-  // // sample GET request:
-  // //"https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
-  // const request = `https://api.edamam.com/search?q=${query}&app_id=${appID}&app_key=${apiKey}`
-  //
-  // useEffect(() => {
-  //   getRecipes()
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [query])
-  //
-  // const getRecipes = async () => {
-  //   const response = await fetch(request)
-  //   const data = await response.json()
-  //   // hits are the individual recipes returned from a query
-  //   setRecipes(data.hits)
-  //   console.log(data.hits);
-  // }
-  //
-  // const updateSearch = e => {
-  //   setSearch(e.target.value)
-  // }
-  //
-  // const getSearch = e => {
-  //   e.preventDefault()
-  //   setQuery(search)
-  //   setSearch('')
-  // }
 
   return (
     <Router>
