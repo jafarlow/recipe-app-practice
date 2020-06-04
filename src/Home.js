59f0b9772050a4d9e253f81e996fa01fdf11b38c
jafarlow <jacobafarlow@gmail.com>
2020-06-04 18:05:37 -0400

    Updated copy on `Home`
    Home.js
      - renamed h1
      - minor change in text to reduce overuse of the word "this"

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <h1>Discover New Recipes</h1>
       <h2>About the app:</h2>
-      <p>This is a recipe search app which uses the Edamam API to help you find new dishes to cook. This is a great way to quickly scan a recipe's ingredients to help you determine if you've found your next favorite dish! Each recipe includes a link to its source page where you can find the cooking instructions. Just imagine, finding the ingredients for a recipe <em><strong>before</strong></em> having to scroll past those stories!</p>
+      <p>This is a recipe search app which uses the Edamam API to help you find new dishes to cook. It's a great way to quickly scan a recipe's ingredients to help you determine if you've found your next favorite dish! Each recipe includes a link to its source page where you can find the cooking instructions. Just imagine, finding the ingredients for a recipe <em><strong>before</strong></em> having to scroll past those stories!</p>
       <p>This app uses the free tier of the Edamam API, which limits searches per minute to 5. If you encounter any difficulty returning recipes from your search, hang tight for a second before initiating another search.</p>
       <p>What are you waiting for? <Link to="/search"> Let's get searching! </Link></p>
     </div>
