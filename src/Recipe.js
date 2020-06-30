01a7bac0651f1d82b3ea86b4fe4b4d7c754ce97b
jafarlow <jacobafarlow@gmail.com>
2020-06-30 07:34:20 -0400

    Including first new element in Recipe
    Recipe.js
      - added `diet` and `health` to destructured props
      - included Health Label(s) & call on `health` prop
        - utilize `.join()` to add comma & space between listed items
        - added `NOTE:` to explain why it's there

--- src/Recipe.js
+++ src/Recipe.js
@@ -6,10 +6,15 @@ import React from 'react'
 // generating a duplicate value in the list is small enough to
 // consider it 0%
 
+// NOTE: using `join()` below adds a comma and a space between items
+// in an array. Otherwise it jams all the items together with no
+// spacing and it looks gross
+
 const target = "_blank"
 const rel = "external nofollow noopener noreferrer"
 
-const Recipe = ({ title, calories, image, ingredients, url, servings }) => {
+const Recipe = ({ title, calories, image, ingredients, url, servings, diet, health }) => {
+
   return (
     <div className="dish">
       <h2>{title}</h2>
@@ -21,6 +26,7 @@ const Recipe = ({ title, calories, image, ingredients, url, servings }) => {
       </ul>
       <p># of servings: {servings}</p>
       <p>Calories per serving: {Math.round(calories / servings)}</p>
+      <p>Health Label(s): {health.join(', ')}</p>
       <p className="source-link">Visit <a href={url} target={target} rel={rel}>recipe source</a> for cooking instructions (external link)</p>
     </div>
   )
