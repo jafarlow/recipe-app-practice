c50c070db3ce11604b242a467424475a21a1df1c
jafarlow <jacobafarlow@gmail.com>
2020-08-13 09:36:26 -0400

    Filling out constructor for Pagination
    Pagination.js
      - `totalRecipes` indicates the total number of recipes to be
        paginated
      - `pageLimit` indicates the number of recipes to be shown per page.
        If not specified, it defaults to 12 as defined in the
        `constructor()`.
      - `pageNeighbours` indicates the number of additional page numbers
        to show on each side of the current page. The minimum value is 0
        and the maximum value is 2.
      - in computing the `totalPages`, use `Math.ceil` so capture
        everything in the last page
      - initialize the state with the `currentPage` property set to 1 to
        internally keep track of the currently active page.

--- src/Pagination.js
+++ src/Pagination.js
@@ -4,6 +4,17 @@ class Pagination extends Component {
   constructor(props) {
     super(props)
     const { totalRecipes = null, pageLimit = 12, pageNeighbors = 1 } = props
+
+    this.pageLimit = typeof pageLimit === 'number' ? pageLimit : 12;
+    this.totalRecipes = typeof totalRecipes === 'number' ? totalRecipes : 0;
+
+    this.pageNeighbors = typeof pageNeighbors === 'number'
+      ? Math.max(0, Math.min(pageNeighbors, 2))
+      : 0;
+
+    this.totalPages = Math.ceil(this.totalRecipes / this.pageLimit)
+
+    this.state ={ currentPage: 1 }
   }
 }
 
