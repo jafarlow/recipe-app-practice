30ee612b23721e7e5db85047fc2c2f1422d7b3bb
jafarlow <jacobafarlow@gmail.com>
2020-08-13 10:59:49 -0400

    Completed the fetchPageNumbers() in Pagination
    Pagination.js
      - completed the `if` statement within `fetchPageNumbers`
      - defined spill, which is the hidden # of pages left or right of the
        current page
      - wrote a switch to test 3 cases:
        - 1 for if pages are hidden left but not right
        - 1 for if pages are hidden right but not left
        - 1 for if pages are hidden on both sides
      - return in the if is an array
      - return outside the if is just 2 numbers

--- src/Pagination.js
+++ src/Pagination.js
@@ -30,7 +30,7 @@ class Pagination extends Component {
 
     this.totalPages = Math.ceil(this.totalRecipes / this.pageLimit)
 
-    this.state ={ currentPage: 1 }
+    this.state = { currentPage: 1 }
   }
 
   fetchPageNumbers = () => {
@@ -40,6 +40,7 @@ class Pagination extends Component {
 
     // total page numbers to show on the nav control
     const totalNumbers = (this.pageNeighbors * 2) + 3
+
     // might need to adjust this number as the tutorial is using 2 pageNeighbors, whereas I want 1
     // using +2 to cover the left & right controls
     const totalBlocks = totalNumbers + 2
@@ -51,8 +52,43 @@ class Pagination extends Component {
       // calling in range declared outside the component
       let pages = range(startPage, endPage)
 
-      
+      // hidden pages to the left
+      const hasLeftSpill = startPage > 2
+
+      // hidden pages to the right
+      const hasRightSpill = (totalPages - endPage) > 1
+
+      // number of hidden pages to the left or right
+      const spillOffset = totalNumbers - (pages.length + 1)
+
+      switch (true) {
+        // if hidden to the left but not the right
+        case (hasLeftSpill && !hasRightSpill): {
+          const extraPages = range((startPage - spillOffset), (startPage - 1))
+          pages = [LEFT_PAGE, ...extraPages, ...pages]
+          break;
+        }
+
+        // if hidden to the right but not the left
+        case (!hasLeftSpill && hasRightSpill): {
+          const extraPages = range((endPage + 1), (endPage + spillOffset))
+          const pages = [...pages, ...extraPages, RIGHT_PAGE]
+          break;
+        }
+
+        // if hidden both to the left and to the right
+        case (hasLeftSpill && hasRightSpill): {
+          pages = [LEFT_PAGE, ...pages, RIGHT_PAGE]
+          break;
+        }
+      }
+
+      // 1st and last page # will always be visible
+      return [1, ...pages, totalPages]
     }
+
+    // return this when the `if` isn't triggered
+    return range(1, totalPages)
   }
 }
 
