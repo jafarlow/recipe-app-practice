16be520f4051efa522c06df8aaf813c5c69b287e
jafarlow <jacobafarlow@gmail.com>
2020-08-13 09:27:49 -0400

    Starting to build in Pagination
    new component, Pagination
      - Wanting to offer more than 12 recipes to the user, so I need to
        build in pagination functionality
      - this will be a great test of my skills in incorporating tactics
        from multiple sources
      - this is setup as a class component instead of as a function
        component
        - i might look into why that is, and if it isn't necessary I might
          switch it over, but for now we're following along with the
          tutorial
      - tutorial source:
    
    https://www.digitalocean.com/community/
    tutorials/how-to-build-custom-pagination-with-react

---
+++ src/Pagination.js
@@ -0,0 +1,10 @@
+import React, { Component, Fragment } from 'react'
+
+class Pagination extends Component {
+  constructor(props) {
+    super(props)
+    const { totalRecipes = null, pageLimit = 12, pageNeighbors = 1 } = props
+  }
+}
+
+export default Pagination
